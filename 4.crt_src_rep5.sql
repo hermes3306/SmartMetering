d42cbdf03e3bd869975a709f0d9430359cdfa545
Joonho Park <joonho.park@hotmail.com>
2017-12-07 10:12:36 +0000

    version 1

--- 4.crt_src_rep5.sql
+++ 4.crt_src_rep5.sql
@@ -1,4 +1,4 @@
-drop replication   rep1;
-create replication rep1 as MASTER
+drop replication   rep5;
+create replication rep5 
 with '192.168.99.21', 20400
 from smssuser.tbl_sm_data to smssuser.tbl_sm_data;
--- 4.crt_src_rep7.sql
+++ 4.crt_src_rep7.sql
@@ -1,4 +1,4 @@
-drop replication   rep1;
-create replication rep1 as MASTER
+drop replication   rep7;
+create replication rep7 
 with '192.168.99.21', 20400
 from smssuser.tbl_sm_data to smssuser.tbl_sm_data;
